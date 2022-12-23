fceae71d5c3aa98aaa0e5329bee2f8a288d8a88c
Andrew Holmes <acholmes001@gmail.com>
2022-12-23 01:52:14 +0000

    Basic mobile navbar

--- main.js
+++ main.js
@@ -3,14 +3,12 @@ document.addEventListener("DOMContentLoaded", function()
     let navDisplayed = false;
     let navItems = document.querySelector("#mobile-nav-items");
     let toggle = document.querySelector("#nav-toggle-icon");
-    console.log(navItems); 
     
     toggle.addEventListener('click', function()
     {
         if(navDisplayed == true)
         {
-            console.log("clicked");
-            navItems.style.display = "none";
+            navItems.style.display = "none ";
             navDisplayed = false;
         }
         else
